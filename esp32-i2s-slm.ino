13ea1fbc1cfe2000002568d41be0db74c14b872e
Benji <asdf.tgz@gmail.com>
2023-03-21 10:10:59 +0100

    Add FastLED support with initial dB thresholds

--- esp32-i2s-slm.ino
+++ esp32-i2s-slm.ino
@@ -40,19 +40,25 @@
 
 #include <driver/i2s.h>
 #include "sos-iir-filter.h"
+#include <FastLED.h>
 
 //
 // Configuration
 //
 
+#define LED_TYPE NEOPIXEL
+#define NUM_LEDS 7                    // How many LEDs are attached to the Arduino?
+#define DATA_PIN 2                    // Which pin on the Arduino is connected to the LEDs?
+CRGB leds[NUM_LEDS];
+int brightness = 20;                  // LED brightness, 0 (min) to 255 (max)
+
 #define LEQ_PERIOD        1           // second(s)
-#define WEIGHTING         C_weighting // Also avaliable: 'C_weighting' or 'None' (Z_weighting)
+#define WEIGHTING         A_weighting // Also avaliable: 'C_weighting' or 'None' (Z_weighting)
 #define LEQ_UNITS         "LAeq"      // customize based on above weighting used
 #define DB_UNITS          "dBA"       // customize based on above weighting used
-#define USE_DISPLAY       1
 
 // NOTE: Some microphones require at least DC-Blocker filter
-#define MIC_EQUALIZER     ICS43434    // See below for defined IIR filters or set to 'None' to disable
+#define MIC_EQUALIZER     INMP441     // See below for defined IIR filters or set to 'None' to disable
 #define MIC_OFFSET_DB     3.0103      // Default offset (sine-wave RMS vs. dBFS). Modify this value for linear calibration
 
 // Customize these values from microphone datasheet
@@ -74,26 +80,12 @@ constexpr double MIC_REF_AMPL = pow(10, double(MIC_SENSITIVITY)/20) * ((1<<(MIC_
 //
 // Below ones are just example for my board layout, put here the pins you will use
 //
-#define I2S_WS            18 
-#define I2S_SCK           23 
-#define I2S_SD            19 
+#define I2S_WS            15
+#define I2S_SCK           14
+#define I2S_SD            32
 
 // I2S peripheral to use (0 or 1)
 #define I2S_PORT          I2S_NUM_0
-
-//
-// Setup your display library (and geometry) here
-// 
-#if (USE_DISPLAY > 0)
-  // ThingPulse/esp8266-oled-ssd1306, you may need the latest source and PR#198 for 64x48
-  #include <SSD1306Wire.h>
-  #define OLED_GEOMETRY     GEOMETRY_64_48
-  //#define OLED_GEOMETRY GEOMETRY_128_32
-  //#define OLED_GEOMETRY GEOMETRY_128_64
-  #define OLED_FLIP_V       1
-  SSD1306Wire display(0x3c, SDA, SCL, OLED_GEOMETRY);
-#endif
-
 
 //
 // IIR Filters
@@ -119,31 +111,6 @@ SOS_IIR_Filter DC_BLOCKER = {
 // NOTE: SOS matrix 'a1' and 'a2' coefficients are negatives of tf2sos output
 //
 
-// TDK/InvenSense ICS-43434
-// Datasheet: https://www.invensense.com/wp-content/uploads/2016/02/DS-000069-ICS-43434-v1.1.pdf
-// B = [0.477326418836803, -0.486486982406126, -0.336455844522277, 0.234624646917202, 0.111023257388606];
-// A = [1.0, -1.93073383849136326, 0.86519456089576796, 0.06442838283825100, 0.00111249298800616];
-SOS_IIR_Filter ICS43434 = { 
-  gain: 0.477326418836803,
-  sos: { // Second-Order Sections {b1, b2, -a1, -a2}
-   {+0.96986791463971267, 0.23515976355743193, -0.06681948004769928, -0.00111521990688128},
-   {-1.98905931743624453, 0.98908924206960169, +1.99755331853906037, -0.99755481510122113}
-  }
-};
-
-// TDK/InvenSense ICS-43432
-// Datasheet: https://www.invensense.com/wp-content/uploads/2015/02/ICS-43432-data-sheet-v1.3.pdf
-// B = [-0.45733702338341309   1.12228667105574775  -0.77818278904413563, 0.00968926337978037, 0.10345668405223755]
-// A = [1.0, -3.3420781082912949, 4.4033694320978771, -3.0167072679918010, 1.2265536567647031, -0.2962229189311990, 0.0251085747458112]
-SOS_IIR_Filter ICS43432 = {
-  gain: -0.457337023383413,
-  sos: { // Second-Order Sections {b1, b2, -a1, -a2}
-    {-0.544047931916859, -0.248361759321800, +0.403298891662298, -0.207346186351843},
-    {-1.909911869441421, +0.910830292683527, +1.790285722826743, -0.804085812369134},
-    {+0.000000000000000, +0.000000000000000, +1.148493493802252, -0.150599527756651}
-  }
-};
-
 // TDK/InvenSense INMP441
 // Datasheet: https://www.invensense.com/wp-content/uploads/2015/02/INMP441.pdf
 // B ~= [1.00198, -1.99085, 0.98892]
@@ -152,32 +119,6 @@ SOS_IIR_Filter INMP441 = {
   gain: 1.00197834654696, 
   sos: { // Second-Order Sections {b1, b2, -a1, -a2}
     {-1.986920458344451, +0.986963226946616, +1.995178510504166, -0.995184322194091}
-  }
-};
-
-// Infineon IM69D130 Shield2Go
-// Datasheet: https://www.infineon.com/dgdl/Infineon-IM69D130-DS-v01_00-EN.pdf?fileId=5546d462602a9dc801607a0e46511a2e
-// B ~= [1.001240684967527, -1.996936108836337, 0.995703101823006]
-// A ~= [1.0, -1.997675693595542, 0.997677044195563]
-// With additional DC blocking component
-SOS_IIR_Filter IM69D130 = {
-  gain: 1.00124068496753,
-  sos: {
-    {-1.0, 0.0, +0.9992, 0}, // DC blocker, a1 = -0.9992
-    {-1.994461610298131, 0.994469278738208, +1.997675693595542, -0.997677044195563}
-  }
-};
-
-// Knowles SPH0645LM4H-B, rev. B
-// https://cdn-shop.adafruit.com/product-files/3421/i2S+Datasheet.PDF
-// B ~= [1.001234, -1.991352, 0.990149]
-// A ~= [1.0, -1.993853, 0.993863]
-// With additional DC blocking component
-SOS_IIR_Filter SPH0645LM4H_B_RB = {
-  gain: 1.00123377961525, 
-  sos: { // Second-Order Sections {b1, b2, -a1, -a2}
-    {-1.0, 0.0, +0.9992, 0}, // DC blocker, a1 = -0.9992
-    {-1.988897663539382, +0.988928479008099, +1.993853376183491, -0.993862821429572}
   }
 };
 
@@ -250,7 +191,7 @@ void mic_i2s_init() {
     mode: i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
     sample_rate: SAMPLE_RATE,
     bits_per_sample: i2s_bits_per_sample_t(SAMPLE_BITS),
-    channel_format: I2S_CHANNEL_FMT_ONLY_LEFT,
+    channel_format: I2S_CHANNEL_FMT_ONLY_RIGHT,
     communication_format: i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
     intr_alloc_flags: ESP_INTR_FLAG_LEVEL1,
     dma_buf_count: DMA_BANKS,
@@ -354,20 +295,23 @@ void mic_i2s_reader_task(void* parameter) {
 // 
 void setup() {
 
+  // LED setup
+  delay(2000);
+  FastLED.addLeds<LED_TYPE, DATA_PIN>(leds, NUM_LEDS);         
+  FastLED.setDither(false);
+  FastLED.setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(brightness);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 400);
+  set_max_power_indicator_LED(13);
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  FastLED.show();
+
   // If needed, now you can actually lower the CPU frquency,
   // i.e. if you want to (slightly) reduce ESP32 power consumption 
   setCpuFrequencyMhz(80); // It should run as low as 80MHz
   
   Serial.begin(112500);
   delay(1000); // Safety
-  
-  #if (USE_DISPLAY > 0)
-    display.init();
-    #if (OLED_FLIP_V > 0)
-      display.flipScreenVertically();
-    #endif
-    display.setFont(ArialMT_Plain_16);
-  #endif
 
   // Create FreeRTOS queue
   samples_queue = xQueueCreate(8, sizeof(sum_queue_t));
@@ -413,42 +357,23 @@ void setup() {
       Serial.printf("%.1f\n", Leq_dB);
 
       // Debug only
-      //Serial.printf("%u processing ticks\n", q.proc_ticks);
-    }
+      // Serial.printf("%u processing ticks\n", q.proc_ticks);
 
-    #if (USE_DISPLAY > 0)
-
-      //
-      // Example code that displays the measured value.
-      // You should customize the below code for your display 
-      // and display library used.
-      //
-      
-      display.clear();
-
-      // It is important to somehow notify when the deivce is out of its range
-      // as the calculated values are very likely with big error
-      if (Leq_dB > MIC_OVERLOAD_DB) {
-        // Display 'Overload' if dB value is over the AOP
-        display.drawString(0, 24, "Overload");
-      } else if (isnan(Leq_dB) || (Leq_dB < MIC_NOISE_DB)) {
-        // Display 'Low' if dB value is below noise floor
-        display.drawString(0, 24, "Low");
+      if (Leq_dB < 40) {
+        // leds[0] = CRGB::Green; FastLED.show();
+        fill_solid( leds, NUM_LEDS, CRGB::Green);
+        FastLED.delay(500);
+      } else if (Leq_dB < 60) {
+        // leds[0] = CRGB::Yellow; FastLED.show();
+        fill_solid( leds, NUM_LEDS, CRGB::Yellow);
+        FastLED.delay(500);
+      } else {
+        // leds[0] = CRGB::Red; FastLED.show();
+        fill_solid( leds, NUM_LEDS, CRGB::Red);
+        FastLED.delay(500);
       }
-      
-      // The 'short' Leq line
-      double short_Leq_dB = MIC_OFFSET_DB + MIC_REF_DB + 20 * log10(sqrt(double(q.sum_sqr_weighted) / SAMPLES_SHORT) / MIC_REF_AMPL);
-      uint16_t len = min(max(0, int(((short_Leq_dB - MIC_NOISE_DB) / MIC_OVERLOAD_DB) * (display.getWidth()-1))), display.getWidth()-1);
-      display.drawHorizontalLine(0, 0, len);
-      display.drawHorizontalLine(0, 1, len);
-      display.drawHorizontalLine(0, 2, len);
-      
-      // The Leq numeric decibels
-      display.drawString(0, 4, String(Leq_dB, 1) + " " + DB_UNITS);
-      
-      display.display();
-      
-    #endif // USE_DISPLAY
+
+    }
   }
 }
 
